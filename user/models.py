7e3d69f3a7d90591d6e5a1df4499790e7c60e716
12moi <moi.shadrack@student.moringaschool.com>
2022-05-10 14:17:25 +0300

    add changes

--- user/models.py
+++ user/models.py
@@ -120,17 +120,18 @@ class Employer(models.Model):
     website = models.URLField(blank=True)
 
 class Applicants(models.Model):
-    Applicant_id = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     Full_Name = models.CharField(max_length=255)
     Email = models.CharField(max_length=255)
     Contact = models.IntegerField()
     Availability= models.CharField(max_length=255)
     Salary_Expectations = models.CharField(max_length=255)
-
+    Relation= models.ForeignKey(UserProfile,  on_delete=models.CASCADE,)
 
 
     def save_Applicants(self):
         self.save()
+
 
     def delete_Applicants(self):
         self.delete()

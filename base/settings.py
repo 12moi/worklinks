040273df186980b7f5f2de83a62bfece07f6c04b
12moi <moi.shadrack@student.moringaschool.com>
2022-05-10 16:15:32 +0300

    add modifications

--- base/settings.py
+++ base/settings.py
@@ -75,36 +75,36 @@ SECRET_KEY = 'django-insecure-fc9tzt)!o0^pjims11h@@xdlba13u(wz7l03rop&t7g6u2q@l=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
-# # MODE=config("MODE", default="dev")
-# # SECRET_KEY = config('SECRET_KEY')
-# # DEBUG = config('DEBUG', default=False, cast=bool)
-# # # development
-# # if config('MODE')=="dev":
-# #    DATABASES = {
-# #        'default': {
-# #            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-# #            'NAME': config('DB_NAME'),
-# #            'USER': config('DB_USER'),
-# #            'PASSWORD': config('DB_PASSWORD'),
-# #            'HOST': config('DB_HOST'),
-# #            'PORT': '',
-# #        }
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
        
-# #    }
-# # # production
-# # else:
-# #    DATABASES = {
-# #        'default': dj_database_url.config(
-# #            default=config('DATABASE_URL')
-# #        )
-# #    }
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
 
-# # db_from_env = dj_database_url.config(conn_max_age=500)
-# # DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-# # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 

107116a1eac27aae50c6eb0c79e16659b6a63aa0
12moi <moi.shadrack@student.moringaschool.com>
2022-05-11 20:46:09 +0300

    add swagger api documentation

--- user/urls.py
+++ user/urls.py
@@ -6,27 +6,24 @@ from rest_framework.routers import DefaultRouter
 from .views import    EmployerProfileView, SignUpViewSet, UpdateUserProfileViewSet
 from rest_framework import routers
 from .views import LogoutView, RegisterView, LoginView, UserView
-# from rest_framework_swagger.views import get_swagger_view
 from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+# schema_view = get_swagger_view(title='Worklinks API')
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Worlinks API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
-# schema_view = get_schema_view(
-#    openapi.Info(
-#       title="Worklinks API",
-#       default_version='v1',
-#       description="Test description",
-#       terms_of_service="https://www.google.com/policies/terms/",
-#       contact=openapi.Contact(email="contact@snippets.local"),
-#       license=openapi.License(name="BSD License"),
-#    ),
-#    public=True,
-#    permission_classes=[permissions.AllowAny],
-#    authentication_classes=[]
-# )
-
-# schema_view = get_swagger_view(title='users API')
 
 router = routers.DefaultRouter()
 router.register('Job', views.JobViewSet)
@@ -41,6 +38,7 @@ router.register('Apply', views.ApplyViewSet)
 
 urlpatterns = [
    path('register', RegisterView.as_view()),
+   
     path('login', LoginView.as_view()),
     path('user', UserView.as_view()),
     path('logout', LogoutView.as_view()),
@@ -57,5 +55,10 @@ urlpatterns = [
     # path('Applicants/', views.Applicants, name='Applicants'),
     path('Apply/', views.Apply, name='Apply'),
     
+
+   path('swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
